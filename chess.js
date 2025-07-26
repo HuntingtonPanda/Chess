303810c286efdfd528a705e864ead4044f1043c1
Huntington <cohuntington7@gmail.com>
2025-07-25 20:40:34 -0700

    Use createElement instead of modifying innerHTML improving efficiency

--- chess.js
+++ chess.js
@@ -1,16 +1,22 @@
 const chessBoard = document.querySelector('.chessBoard');
 let squareName;
-let isLight = true;
+let isLight;
 
-for (let column = 1; column <= 8; column++) {
+for (let column = 8; column >= 1; column--) {
   for (let row = 1; row <= 8; row++) {
-    squareName = String.fromCharCode(65 + (row - 1)) + (9 - column);
+    const square = document.createElement('div');
+    squareName = String.fromCharCode(65 + (row - 1)) + column;
 
-    chessBoard.innerHTML += isLight
-      ? "<div class='square light' data-square='" + squareName + "'>" + squareName + "</div>"
-      : "<div class='square dark' data-square='" + squareName + "'>" + squareName + "</div>";
+    //Setting up class
+    isLight = (row + column) % 2 === 0;
+    square.classList.add('square');
+    square.classList.add(isLight ? 'light' : 'dark');
 
-    isLight = !isLight;
+    //Setting data attribute
+    square.setAttribute('data-square', squareName);
+    
+    square.textContent = squareName;
+
+    chessBoard.appendChild(square);
   }
-  isLight = !isLight;
 }
