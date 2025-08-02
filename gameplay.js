3cfb9421ef581344f5bd7f83b13d4b2a0dd47d15
Huntington <cohuntington7@gmail.com>
2025-08-01 21:47:08 -0700

    You can select an unselected piece

--- gameplay.js
+++ gameplay.js
@@ -151,6 +151,7 @@ function selectPiece(square){
     while(possibleMoves.length > 0){
       possibleMoves.pop().classList.remove('possibleMove');
     }
+    selectedPiece = null;
     return;
   }
 
